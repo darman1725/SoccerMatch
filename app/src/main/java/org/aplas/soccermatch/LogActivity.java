cc8b871ac96ce0272ebcd190910d114ed0b7cd2d
darman1725 <darmansaragih1701@gmail.com>
2021-10-13 08:36:52 +0700

    Menyesuaikan Program

--- app/src/main/java/org/aplas/soccermatch/LogActivity.java
+++ app/src/main/java/org/aplas/soccermatch/LogActivity.java
@@ -19,6 +19,59 @@ public class LogActivity extends AppCompatActivity {
     private RecyclerView logView;
     private Button restartBtn;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log);
 
+        logView = (RecyclerView) findViewById(R.id.logRcView);
+        restartBtn = (Button) findViewById(R.id.restartBtn);
 
+        logView.setLayoutManager(new LinearLayoutManager(this));
+        logView.setItemAnimator(new DefaultItemAnimator());
+        logView.setHasFixedSize(false);
+
+        restartBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMainActivity();
+            }
+        });
+    }
+
+    private void loadHeader() {
+        TextView matchResultTxt = (TextView) findViewById(R.id.matchResultTxt);
+        TextView matchScoreTxt = (TextView) findViewById(R.id.matchScoreTxt);
+        matchResultTxt.setText("Result: " + getIntent().getStringExtra("MATCH_RESULT"));
+        matchScoreTxt.setText("Score: " + getIntent().getStringExtra("MATCH_SCORE"));
+    }
+
+    private void loadLogData() {
+        ArrayList<LogItem> itemList = new ArrayList<>();
+        LogAdapter itemArrayAdapter = new LogAdapter(R.layout.layout_log, itemList);
+        itemArrayAdapter.setHasStableIds(true);
+        logView.setAdapter(itemArrayAdapter);
+        ArrayList<String> eventList = getIntent().getStringArrayListExtra("MATCH_EVENT");
+        for (int i = 0; i < eventList.size(); i++) {
+            String[] data = eventList.get(i).split("@");
+            String eventName = data[0];
+            String eventTime = data[3];
+            String eventPlayer = data[1] + " (" + data[2] + ")";
+            itemList.add(new LogItem(eventName, eventTime, eventPlayer));
+        }
+        Toast.makeText(LogActivity.this, "Data loaded", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        loadHeader();
+        loadLogData();
+    }
+
+    private void openMainActivity() {
+        Intent main = new Intent(getApplicationContext(), MainActivity.class);
+        main.putExtra("MESSAGE", "new");
+        startActivity(main);
+    }
 }
