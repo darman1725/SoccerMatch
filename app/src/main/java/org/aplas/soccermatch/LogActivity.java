77dcd0e17bf40a49c4307419bc175c4f22a6e767
darman1725 <darmansaragih1701@gmail.com>
2021-10-13 08:34:44 +0700

    Mendeklarasikan Semua Variabel

---
+++ app/src/main/java/org/aplas/soccermatch/LogActivity.java
@@ -0,0 +1,32 @@
+package org.aplas.soccermatch;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class LogActivity extends AppCompatActivity {
+
+    private RecyclerView logView;
+    private Button restartBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log);
+
+
+
+
+    }
+
+}
